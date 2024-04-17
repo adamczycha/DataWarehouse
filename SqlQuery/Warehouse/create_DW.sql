e115f337b384e84e8e1c9ec529e0c41d4452a9fe
jakmachura <jakmachura@gmail.com>
2024-04-17 21:12:24 +0200

    Refractoring SQL inserts

--- SqlQuery/Warehouse/create_DW.sql
+++ SqlQuery/Warehouse/create_DW.sql
@@ -1,68 +1,60 @@
 use [call_masterDW]
 
 CREATE TABLE [Department] (
-  [ID_Department] Int,
-  [Name] varchar(50),
-  PRIMARY KEY ([ID_Department])
+  [ID_Department] Int IDENTITY PRIMARY KEY,
+  [Name] varchar(50)
 );
 
 CREATE TABLE [Employees] (
-  [ID_Employee] Int,
+  [ID_Employee] Int IDENTITY PRIMARY KEY,
   [ID_Department] Int,
   [Name] varchar(20),
   [Address] varchar(50),
   [Position] varchar(50),
-  PRIMARY KEY ([ID_Employee]),
   FOREIGN KEY ([ID_Department]) REFERENCES [Department]([ID_Department])
 );
 
 CREATE TABLE [JUNK] (
-  [ID_Junk] int,
+  [ID_Junk] Int IDENTITY PRIMARY KEY,
   [Priority] nvarchar (255) NOT NULL CHECK ([Priority] IN('Low', 'Medium','High')),
   [Escalated] nvarchar (255) NOT NULL CHECK ([Escalated] IN('True', 'False')),
-  [ResolutionType] nvarchar (255) NOT NULL CHECK ([ResolutionType] IN('Processing', 'Finished')),
-  PRIMARY KEY ([ID_Junk])
+  [ResolutionType] nvarchar (255) NOT NULL CHECK ([ResolutionType] IN('Processing', 'Finished'))
 );
 
 
 
 CREATE TABLE [Date] (
-  [ID_creation_date] Int,
+  [ID_creation_date] Int IDENTITY PRIMARY KEY,
   [Creation_Day] int,
   [Creation_Year] int,
-  [Creation_Month] int,
-  PRIMARY KEY ([ID_creation_date])
+  [Creation_Month] int
 );
 
 CREATE TABLE [TicketDescription] (
-  [ID_Description] Int,
+  [ID_Description] Int IDENTITY PRIMARY KEY,
   [Notes] varchar(255),
   [Description] varchar(255),
-  [Title] varchar(100),
-  PRIMARY KEY ([ID_Description])
+  [Title] varchar(100)
 );
 
 CREATE TABLE [Customer] (
-  [ID_Customer] Int,
+  [ID_Customer] Int IDENTITY PRIMARY KEY,
   [Name] varchar(20),
   [E-mail] varchar(100),
-  [Phone] varchar(12),
-  PRIMARY KEY ([ID_Customer])
+  [Phone] varchar(12)
 );
 
 CREATE TABLE [Category] (
-  [ID_Category] Int,
-  [Name] varchar(50),
-  PRIMARY KEY ([ID_Category])
+  [ID_Category] Int IDENTITY PRIMARY KEY,
+  [Name] varchar(50)
 );
 
 CREATE TABLE [Feedback] (
-  [ID_feedback] int,
+  [ID_feedback] Int IDENTITY PRIMARY KEY,
   [Satisfaction Level] nvarchar (255) NOT NULL CHECK ([Satisfaction Level] IN('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')),
   [Feedback] varchar(255),
   [Follow-up Required] nvarchar (255) NOT NULL CHECK ([Follow-up Required] IN('Yes', 'No')),
-  [Customer Location ] varchar(255),
-  PRIMARY KEY ([ID_feedback])
+  [Customer Location ] varchar(255)
 );
 
 
