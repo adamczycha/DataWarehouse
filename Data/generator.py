25855f69d98c419c2e1b260d204cd892df344ea5
Jakub Machura <110567207+Machmurka@users.noreply.github.com>
2024-05-15 22:08:52 +0200

    Add files via upload

--- Data/generator.py
+++ Data/generator.py
@@ -1,11 +1,17 @@
+
 from faker import Faker
+from tkinter import messagebox
 from faker.providers import DynamicProvider 
 from random import random, sample
 import numpy as np
 import csv
 from datetime import date
+import tkinter as tk
 import os
-import multiprocessing
+from tkinter import PhotoImage, Label
+from PIL import Image, ImageTk
+import pickle
+
 fake=Faker()
 
 
@@ -15,29 +21,31 @@ class Data(Faker):
         self.Sample_size=5
         self.Ticket_Description_Chars=30
 
-        
-        self.Ticket_Number=             513501
-        self.TicketResolution_Number=   513501
-        self.Customer_Number=           10185
-        self.Employees_Number=          1956
+        #ilość wierszy w danej tabeli w danym TimeLine
 
+        # self.Ticket_Number=             100000
+        # self.TicketResolution_Number=   100000
+        # self.Customer_Number=           5000
+        # self.Employees_Number=          500
 
-        self.Ticket_NumberT2=           551857
-        self.TicketResolution_NumberT2= 551857
-        self.Customer_NumberT2=         8154
-        self.Employees_NumberT2=        1211
+        self.Ticket_Number = 0
+        self.TicketResolution_Number = 0
+        self.Customer_Number = 0
+        self.Employees_Number = 0
 
+        self.Ticket_NumberT2=           0
+        self.TicketResolution_NumberT2= 0
+        self.Customer_NumberT2=         0
+        self.Employees_NumberT2=        0
     #Zapisywanie do pliku CSV
         
     def to_csv(self, data_dict, filename):
         output_folder = "output"
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
-        filename = os.path.join(output_folder, filename)
-        with open(filename, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=data_dict.keys())
-            writer.writeheader()
-            writer.writerows([dict(zip(data_dict, d)) for d in zip(*data_dict.values())])
+        filename = os.path.join(output_folder, filename + '.bulk')
+        with open(filename, 'wb') as bulkfile:
+            pickle.dump([dict(zip(data_dict, d)) for d in zip(*data_dict.values())], bulkfile)
 
 
     def Department(self)->None:
@@ -61,14 +69,14 @@ class Data(Faker):
         self.Department_departmentID=[fake.unique.random_int(1,self.Department_size) for i in range (self.Department_size)]
         
 
-        #print(f"\n\n DEPARTMENT SAMPLE HEAD({self.Sample_size})\n\n Department Names {self.Department_list[self.Sample_size:]} \n Department ID {self.Department_departmentID[self.Sample_size:]}")
+        print(f"\n\n DEPARTMENT SAMPLE HEAD({self.Sample_size})\n\n Department Names {self.Department_list[self.Sample_size:]} \n Department ID {self.Department_departmentID[self.Sample_size:]}")
     
     def DepartmentBulk(self)->None:
         data_dict = {
             'DepartmentID': self.Department_departmentID,
             'Name': self.Department_list
         }
-        self.to_csv(data_dict, 'Department.csv')
+        self.to_csv(data_dict, 'Department')
 
 
     def Employees(self)->None:
@@ -98,7 +106,7 @@ class Data(Faker):
 
         self.Employee_departmentID=[np.random.choice(self.Department_departmentID) for i in range(self.Employees_Number)]
  
-        #print(f"\n\n EMPLOYEE SAMPLE HEAD({self.Sample_size})\n\n EmployeeID {self.Employees_EmployeeID[:self.Sample_size]} \n Name {self.Employee_Name[:self.Sample_size]} \n Address {self.Employee_Address[:self.Sample_size]} \n Position {self.Employee_Position[:self.Sample_size]}\n DepartmentID {self.Employee_departmentID[self.Sample_size:]}")
+        print(f"\n\n EMPLOYEE SAMPLE HEAD({self.Sample_size})\n\n EmployeeID {self.Employees_EmployeeID[:self.Sample_size]} \n Name {self.Employee_Name[:self.Sample_size]} \n Address {self.Employee_Address[:self.Sample_size]} \n Position {self.Employee_Position[:self.Sample_size]}\n DepartmentID {self.Employee_departmentID[self.Sample_size:]}")
 
 
         # T2
@@ -122,7 +130,7 @@ class Data(Faker):
         'Position': self.Employee_Position,
         'DepartmentID': self.Employee_departmentID
         }
-        self.to_csv(data_dict, 'Employees.csv')
+        self.to_csv(data_dict, 'Employees')
 
         data_dict = {
         'EmployeeID': self.Employees_EmployeeIDT2,
@@ -131,7 +139,7 @@ class Data(Faker):
         'Position': self.Employee_PositionT2,
         'DepartmentID': self.Employee_departmentIDT2
         }
-        self.to_csv(data_dict, 'EmployeesT2.csv')
+        self.to_csv(data_dict, 'EmployeesT2')
 
     def Customer(self)->None:
 
@@ -146,7 +154,7 @@ class Data(Faker):
 
         self.Customer_Phone=[fake.phone_number()[:10] for i in range(self.Customer_Number)]
 
-        #print(f"\n\n CUSTOMER SAMPLE HEAD \n\n CustomerID{self.Customer_CustomerID[:self.Sample_size]} \n Name{self.Customer_Name[:self.Sample_size]} \n E-mail {self.Customer_Email[:self.Sample_size]} \n Phone {self.Customer_Phone[:self.Sample_size]}" )
+        print(f"\n\n CUSTOMER SAMPLE HEAD \n\n CustomerID{self.Customer_CustomerID[:self.Sample_size]} \n Name{self.Customer_Name[:self.Sample_size]} \n E-mail {self.Customer_Email[:self.Sample_size]} \n Phone {self.Customer_Phone[:self.Sample_size]}" )
     
         # T2
         self.Customer_CustomerIDT2=sample(range(self.Customer_Number+1,self.Customer_Number+self.Customer_NumberT2+1),self.Customer_NumberT2)
@@ -164,7 +172,7 @@ class Data(Faker):
         'E-mail': self.Customer_Email,
         'Phone': self.Customer_Phone
         }
-        self.to_csv(data_dict, 'Customer.csv')
+        self.to_csv(data_dict, 'Customer')
 
         data_dict = {
         'CustomerID': self.Customer_CustomerIDT2,
@@ -172,7 +180,7 @@ class Data(Faker):
         'E-mail': self.Customer_EmailT2,
         'Phone': self.Customer_PhoneT2,
         }
-        self.to_csv(data_dict, 'CustomerT2.csv')
+        self.to_csv(data_dict, 'CustomerT2')
 
 
     def Category(self)->None:
@@ -181,13 +189,14 @@ class Data(Faker):
             'Hardware Issues',
             'Network Connectivity Problems',
             'Email Configuration and Troubleshooting',
-            '#printer and Peripheral Setup/Issues',
+            'Printer and Peripheral Setup/Issues',
             'Account Access and Permissions',
             'Password Resets',
             'Application Errors',
+
             'Virus and Malware Removal',
             'Data Backup and Restoration',
-            'System Performance Degradation',
+            'System Performance Degradation',   
             'Mobile Device Setup and Troubleshooting',
             'Software Installation and Updates',
             'Security Concerns',
@@ -202,7 +211,7 @@ class Data(Faker):
 
         self.Category_CategoryID = sample(range(1, self.Category_Number + 1), self.Category_Number)
 
-        #print(f"\n\n Category SAMPLE HEAD({self.Sample_size})\n\n Category Names {self.Category_Name[:self.Sample_size]} \n Category ID {self.Category_CategoryID[:self.Sample_size]}")
+        print(f"\n\n Category SAMPLE HEAD({self.Sample_size})\n\n Category Names {self.Category_Name[:self.Sample_size]} \n Category ID {self.Category_CategoryID[:self.Sample_size]}")
         
     def CategoryBulk(self)->None:
 
@@ -210,7 +219,7 @@ class Data(Faker):
         'CategoryID': self.Category_CategoryID,
         'Name': self.Category_Name
         }
-        self.to_csv(data_dict, 'Category.csv')
+        self.to_csv(data_dict, 'Category')
 
 
     def Ticket(self)->None:
@@ -252,7 +261,7 @@ class Data(Faker):
 
         self.Ticket_CategoryID=[np.random.choice(self.Category_CategoryID) for i in range(self.Ticket_Number)]
 
-        #print(f"\n\n Ticket SAMPLE HEAD \n\n TicketID{self.Ticket_TicketID[:self.Sample_size]} \n Title{self.Ticket_Title[:self.Sample_size]} \n Description {self.Ticket_Description[:self.Sample_size]} \n Priority {self.Ticket_Priority[:self.Sample_size]}\n CustomerID {self.Ticket_CustomerID[:self.Sample_size]} \n CategoryID {self.Ticket_CategoryID[:self.Sample_size]}" )
+        print(f"\n\n Ticket SAMPLE HEAD \n\n TicketID{self.Ticket_TicketID[:self.Sample_size]} \n Title{self.Ticket_Title[:self.Sample_size]} \n Description {self.Ticket_Description[:self.Sample_size]} \n Priority {self.Ticket_Priority[:self.Sample_size]}\n CustomerID {self.Ticket_CustomerID[:self.Sample_size]} \n CategoryID {self.Ticket_CategoryID[:self.Sample_size]}" )
         
         # T2
         self.Ticket_TicketIDT2 = sample(range(self.Ticket_Number + 1, self.Ticket_NumberT2+self.Ticket_Number + 1), self.Ticket_NumberT2)
@@ -279,7 +288,7 @@ class Data(Faker):
         'CustomerID': self.Ticket_CustomerID,
         'CategoryID': self.Ticket_CategoryID
         }
-        self.to_csv(data_dict, 'Ticket.csv')
+        self.to_csv(data_dict, 'Ticket')
 
         data_dict = {
         'TicketID': self.Ticket_TicketIDT2,
@@ -289,7 +298,7 @@ class Data(Faker):
         'CustomerID': self.Ticket_CustomerIDT2,
         'CategoryID': self.Ticket_CategoryIDT2
         }
-        self.to_csv(data_dict, 'TicketT2.csv')
+        self.to_csv(data_dict, 'TicketT2')
 
 
     def TicketResolution(self)->None:
@@ -319,7 +328,7 @@ class Data(Faker):
 
         self.TicketResolution_Notes=[fake.text(max_nb_chars=self.Ticket_Description_Chars) for i in range(self.TicketResolution_Number)]
 
-        #print(f"\n\n TICKET RESOLUTION SAMPLE HEAD \n\n ResolutionID{self.TicketResolution_ResolutionID[:self.Sample_size]} \n TicketID{self.TicketResolution_TicketID[:self.Sample_size]} \n EmployeeId {self.TicketResolution_EmployeeId[:self.Sample_size]} \n ResolutionType {self.TicketResolution_ResolutionType[:self.Sample_size]} \n Escalated {self.TicketResolution_Escalated[:self.Sample_size]} \n CreationDate {self.TicketResolution_CreationDate[:self.Sample_size]} \n ResponseTime {self.TicketResolution_ResponseTime[:self.Sample_size]} \n ResolutionTime {self.TicketResolution_ResolutionTime[:self.Sample_size]}")
+        print(f"\n\n TICKET RESOLUTION SAMPLE HEAD \n\n ResolutionID{self.TicketResolution_ResolutionID[:self.Sample_size]} \n TicketID{self.TicketResolution_TicketID[:self.Sample_size]} \n EmployeeId {self.TicketResolution_EmployeeId[:self.Sample_size]} \n ResolutionType {self.TicketResolution_ResolutionType[:self.Sample_size]} \n Escalated {self.TicketResolution_Escalated[:self.Sample_size]} \n CreationDate {self.TicketResolution_CreationDate[:self.Sample_size]} \n ResponseTime {self.TicketResolution_ResponseTime[:self.Sample_size]} \n ResolutionTime {self.TicketResolution_ResolutionTime[:self.Sample_size]}")
     
 
 
@@ -357,7 +366,7 @@ class Data(Faker):
         'ResolutionTime': self.TicketResolution_ResolutionTime,
         'Notes': self.TicketResolution_Notes
         }
-        self.to_csv(data_dict, 'TicketResolution.csv')
+        self.to_csv(data_dict, 'TicketResolution')
 
         data_dict = {
         'ResolutionID': self.TicketResolution_ResolutionIDT2,
@@ -370,7 +379,7 @@ class Data(Faker):
         'ResolutionTime': self.TicketResolution_ResolutionTimeT2,
         'Notes': self.TicketResolution_NotesT2
         }
-        self.to_csv(data_dict, 'TicketResolutionT2.csv')
+        self.to_csv(data_dict, 'TicketResolutionT2')
 
     def AdditionalSourceCSV(self):
         self.AdditionalSourceCSV_FollowUpRequired_list=['Yes','No']
@@ -401,7 +410,7 @@ class Data(Faker):
         'Customer Location ': self.AdditionalSourceCSV_CustomerLocation,
         'Ticket ID': self.AdditionalSourceCSV_TicketID
         }
-        self.to_csv(data_dict, 'AdditionalSourcesCSV.csv')
+        self.to_csv(data_dict, 'AdditionalSourcesCSV')
 
         data_dict = {
         'FeedbackID': self.AdditionalSourceCSV_FeedbackIDT2,
@@ -411,24 +420,100 @@ class Data(Faker):
         'Customer Location ': self.AdditionalSourceCSV_CustomerLocationT2,
         'Ticket ID': self.AdditionalSourceCSV_TicketIDT2
         }
-        self.to_csv(data_dict, 'AdditionalSourcesT2CSV.csv')
+        self.to_csv(data_dict, 'AdditionalSourcesT2CSV')
+    
+    def LoadAll(self):
+        self.Department()
+        self.DepartmentBulk()
+        self.Employees()
+        self.EmployeesBulk()
+        self.Customer()
+        self.CustomerBulk()
+        self.Category()
+        self.CategoryBulk()
+        self.Ticket()
+        self.TicketBulk()
+        self.TicketResolution()
+        self.TicketResolutionBulk()
+        self.AdditionalSourceCSV()
+        self.AdditionalSourceCSVBulk()
 
+
+class GeneratorGUI:
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Data Generator")
+
+        self.data_generator = Data()
+
+
+        # Create labels and entry fields for each parameter
+        self.ticket_number_label = tk.Label(window, text="Number of rows for Ticket table")
+        self.ticket_number_label.pack()
+        self.ticket_number_entry = tk.Entry(window)
+        self.ticket_number_entry.pack()
+
+        self.ticket_resolution_number_label = tk.Label(window, text="Number of rows for Ticket Resolution table ")
+        self.ticket_resolution_number_label.pack()
+        self.ticket_resolution_number_entry = tk.Entry(window)
+        self.ticket_resolution_number_entry.pack()
+
+        self.customer_number_label = tk.Label(window, text="Number of rows for Customer Resolution table ")
+        self.customer_number_label.pack()
+        self.customer_number_entry = tk.Entry(window)
+        self.customer_number_entry.pack()
+
+        self.employees_number_label = tk.Label(window, text="Number of rows for Employees Resolution table ")
+        self.employees_number_label.pack()
+        self.employees_number_entry = tk.Entry(window)
+        self.employees_number_entry.pack()
+        
+        # Create a button to generate data
+        self.generate_button = tk.Button(window, text="Generate Data", command=self.generate_data)
+        self.generate_button.pack()
+        image = Image.open("CallCenter.png")
+        photo = ImageTk.PhotoImage(image)
+
+        image_label = tk.Label(window, image=photo)
+        image_label.image = photo  # Keep a reference to the image
+
+# Place the label under the button
+        image_label.pack()
+        
+    def generate_data(self):
+        # Get values from entry fields
+        ticket_number = self.ticket_number_entry.get()
+        ticket_resolution_number = self.ticket_resolution_number_entry.get()
+        customer_number = self.customer_number_entry.get()
+        employees_number = self.employees_number_entry.get()
+
+        # Check if the inputs are strings of digits
+        if not (ticket_number.isdigit() and ticket_resolution_number.isdigit() and 
+                customer_number.isdigit() and employees_number.isdigit()):
+            messagebox.showerror("Error", "All inputs must be integers.")
+            return
+
+        # Convert the inputs to integers
+        self.data_generator.Ticket_Number =             int(ticket_number)
+        self.data_generator.TicketResolution_Number =   int(ticket_resolution_number)
+        self.data_generator.Customer_Number =           int(customer_number)
+        self.data_generator.Employees_Number =          int(employees_number)
+
+        self.data_generator.Ticket_NumberT2=          int(ticket_number)
+        self.data_generator.TicketResolution_NumberT2= int(ticket_resolution_number)
+        self.data_generator.Customer_NumberT2=        int(customer_number)
+        self.data_generator.Employees_NumberT2=       int(employees_number)
+        # Generate data
+        self.data_generator.LoadAll()
+
+        messagebox.showinfo("Success", "Data successfully generated!")
+
+window = tk.Tk()
+window.geometry("500x500")
+app = GeneratorGUI(window)
+window.mainloop()
 data=Data()
 
-data.Department()
-data.DepartmentBulk()
-data.Employees()
-data.EmployeesBulk()
-data.Customer()
-data.CustomerBulk()
-data.Category()
-data.CategoryBulk()
-data.Ticket()
-data.TicketBulk()
-data.TicketResolution()
-data.TicketResolutionBulk()
-data.AdditionalSourceCSV()
-data.AdditionalSourceCSVBulk()
 
 
 
